fdc6c6cf16c7c0fb2c8c3f0c2b5588c3686a45b2
unknown <rodolfhoazevedo@gmail.com>
2021-10-22 19:24:31 -0300

    loadSurveysController done

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -2,6 +2,7 @@ import { SurveyModel } from '../../../../domain/models/survey';
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys';
 import { Controller } from '../../../protocols/controller';
 import { LoadSurveysController } from './load-surveys-controller';
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper';
 import MockDate from 'mockdate';
 
 const makeFakeSurveys = (): SurveyModel[] => {
@@ -64,5 +65,25 @@ describe('LoadSurveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveys, 'load');
     await sut.handle({});
     expect(loadSpy).toHaveBeenCalled();
+  });
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(ok(makeFakeSurveys()));
+  });
+
+  test('Should return 204 if loadSurveys returns ampty', async () => {
+    const { sut, loadSurveys } = makeSut();
+    jest.spyOn(loadSurveys, 'load').mockResolvedValueOnce(Promise.resolve([]));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(noContent());
+  });
+
+  test('Should return 500 if loadSurveys throws', async () => {
+    const { sut, loadSurveys } = makeSut();
+    jest.spyOn(loadSurveys, 'load').mockImplementationOnce(() => Promise.reject(new Error()));
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,4 +1,5 @@
 import { LoadSurveys } from "../../../../domain/usecases/load-surveys";
+import { noContent, ok, serverError } from "../../../helpers/http/http-helper";
 import { Controller } from "../../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../../protocols/http";
 
@@ -6,7 +7,11 @@ export class LoadSurveysController implements Controller {
   constructor(private readonly loadSurveys: LoadSurveys) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load();
-    return null;
+    try {
+      const surveys = await this.loadSurveys.load();
+      return surveys.length ? ok(surveys) : noContent();
+    } catch(error) {
+      return serverError(error);
+    }
   }
 }
